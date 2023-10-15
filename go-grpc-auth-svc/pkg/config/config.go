9fb9ffac58c492cbe332581f0e8ccc77475da4a6
Nadeem1815 <nadeemf408@gmail.com>
2023-10-15 15:41:34 +0530

    env and config file

--- go-grpc-auth-svc/pkg/config/config.go
+++ go-grpc-auth-svc/pkg/config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Port         string `mapstructure:"PORT"`
+	DBUrl        string `mapstructure:"db_url"`
+	JWTSecretKey string `mapstructure:"JWT_SECRET_KEY"`
+}
+
+func LoadConfig() (Config Config, err error) {
+	viper.AddConfigPath("./pkg/config/envs")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+
+	}
+	err = viper.Unmarshal(&Config)
+	return
+}
