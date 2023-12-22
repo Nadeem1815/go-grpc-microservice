a3d7f553949c2fed429ce65b2ec845cc984eae10
Nadeem1815 <nadeemf408@gmail.com>
2023-12-22 09:54:53 +0530

    data models added

--- go-grpc-product-svc/cmd/main.go
+++ go-grpc-product-svc/cmd/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+
+	"github.com/Nadeem1815/go-grpc-product-svc/pkg/config"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+	// lis,err:=net.Listen("tcp",c.Port)
+	// if err!=nil {
+	// 	return
+
+	// }
+	// r := gin.Default()
+	// r.Run(c.Port)
+}
