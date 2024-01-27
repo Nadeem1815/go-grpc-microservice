528e10bea7afc083a3174d0fc60a28f67553d556
Nadeem1815 <nadeemf408@gmail.com>
2024-01-27 12:24:22 +0530

    product service add

--- go-grpc-product-svc/cmd/main.go
+++ go-grpc-product-svc/cmd/main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 
+	// "github.com/Nadeem1815/go-grpc-api-gateway/pkg/product/pb"
+	// "github.com/Nadeem1815/go-grpc-auth-svc/pkg/db"
 	"github.com/Nadeem1815/go-grpc-product-svc/pkg/config"
-	"github.com/gin-gonic/gin"
+	"github.com/Nadeem1815/go-grpc-product-svc/pkg/db"
+	"github.com/Nadeem1815/go-grpc-product-svc/pkg/pb"
+	"github.com/Nadeem1815/go-grpc-product-svc/pkg/services"
+
+	// "github.com/Nadeem1815/go-grpc-product-svc/pkg/config"
+	"google.golang.org/grpc"
 )
+
+// func main() {
+// 	c, err := config.LoadConfig()
+
+// 	if err != nil {
+// 		log.Fatalln("Failed at config", err)
+// 	}
+// 	lis, err := net.Listen("tcp", c.Port)
+// 	if err != nil {
+// 		return
+
+// 	}
+// 	r := gin.Default()
+// 	r.Run(c.Port)
+// }
 
 func main() {
 	c, err := config.LoadConfig()
@@ -13,11 +37,25 @@ func main() {
 	if err != nil {
 		log.Fatalln("Failed at config", err)
 	}
-	// lis,err:=net.Listen("tcp",c.Port)
-	// if err!=nil {
-	// 	return
 
-	// }
-	// r := gin.Default()
-	// r.Run(c.Port)
+	h := db.Init(c.DBUrl)
+
+	lis, err := net.Listen("tcp", c.Port)
+
+	if err != nil {
+		log.Fatalln("Failed to listing:", err)
+	}
+
+	fmt.Println("Product Svc on", c.Port)
+
+	s := services.Server{
+		H: h,
+	}
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterProductServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalln("Failed to serve:", err)
+	}
 }
